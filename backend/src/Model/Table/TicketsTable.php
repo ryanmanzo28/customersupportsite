d70e3d3598d8c9d2a38ce722168c1959de75465a
ryanmanzo28 <ryan.manzo@hydracor.net>
2026-07-14 09:02:09 -0400

    improved project skeleton

---
+++ backend/src/Model/Table/TicketsTable.php
@@ -0,0 +1,65 @@
+<?php
+    declare(strict_types=1);
+
+    namespace App\Model\Table;
+
+    use Cake\ORM\RulesChecker;
+    use Cake\ORM\Table;
+    use Cake\Validation\Validator;
+
+    class TicketsTable extends Table
+    {
+        public function initialize(array $config): void
+        {
+            parent::initialize($config);
+
+            $this->setTable('tickets');
+            $this->setDisplayField('title');
+            $this->setPrimaryKey('id');
+
+            $this->addBehavior('Timestamp');
+
+            $this->belongsTo('Users', [
+                'foreignKey' => 'submitting_user_id',
+                'joinType' => 'INNER',
+            ]);
+        }
+
+        public function validationDefault(Validator $validator): Validator
+        {
+            $validator
+                ->integer('id')
+                ->allowEmptyString('id', null, 'create');
+
+            $validator
+                ->integer('submitting_user_id')
+                ->requirePresence('submitting_user_id', 'create')
+                ->notEmptyString('submitting_user_id');
+
+            $validator
+                ->scalar('status')
+                ->maxLength('status', 50)
+                ->requirePresence('status', 'create')
+                ->notEmptyString('status');
+
+            $validator
+                ->scalar('title')
+                ->maxLength('title', 255)
+                ->requirePresence('title', 'create')
+                ->notEmptyString('title');
+
+            $validator
+                ->scalar('body')
+                ->requirePresence('body', 'create')
+                ->notEmptyString('body');
+
+            return $validator;
+        }
+
+        public function buildRules(RulesChecker $rules): RulesChecker
+        {
+            $rules->add($rules->existsIn(['submitting_user_id'], 'Users'));
+
+            return $rules;
+        }
+    }
